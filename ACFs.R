c74e6305384e67e179e19882fd16b3bf42882672
Malvick17 <Malvick17@users.noreply.github.com>
2019-12-02 21:45:01 -0500

    Updated ACF

--- ACFs.R
+++ ACFs.R
@@ -13,7 +13,7 @@ for (j in c(1:8)) {
   } 
 }
 
-par(mfrow = c(4,4))
+par(mfrow = c(4,2))
 for (j in c(1:8)) {
   print(j)
   plot((my_acf[1,j,]/max(my_acf[1,j,]))~seq(1,1000,1),
@@ -55,20 +55,30 @@ my_ccf<-fcs(x = responses[,1,1], y = responses[,2,1], nPoints = 1000)
 plot(my_ccf~seq(1,1000,1), type = "l")
 my_ccf<-array(data = NA,dim = c(8,1000))
 for (j in c(1:8)) {
-  my_ccf[j,]<-fcs(x = responses[,1,j],y = responses[,2,j],nPoints = 1000)
+  # my_ccf[j,]<-fcs(x = responses[,1,j],y = responses[,2,j],nPoints = 1000)
   # plot((my_acf[i,j,]/max(my_acf[i,j,]))~seq(1,1000,1), 
   #      type = "l", 
   #      # col = "grey",
   #      main = paste("Run ",i," Cell", j),
   #      ylab = "Adjusted Autocorrelation",
   #      xlab = "T")
-  plot((my_ccf[j,])~seq(1,1000,1), 
+  plot((my_ccf[j,])/max(my_ccf[j,])~seq(1,1000,1), 
        type = "l", 
-       # col = "grey",
+       col = "dodgerblue4",
        main = paste(" Cell", j),
        ylab = "Adjusted Cross-correlation",
        xlab = "T")
-  abline(h = 0.5, col = "red")
-  print(head(my_ccf[j,1:25]/max(my_ccf[j,])))
+  abline(h = 0, col = "red")
+  abline(h = 0.5, col = "darkgrey")
+  plot((my_ccf[j,1:15]/max(my_ccf[j,]))~seq(1,15,1), 
+       type = "l", 
+       col = "dodgerblue4",
+       main = paste("Cell", j, "CCF (first 15 points)"),
+       ylab = "Adjusted Cross-correlation",
+       # col = "darkgreen",
+       xlab = "T")
+  abline(h = 0, col = "red")
+  abline(h = 0.5, col = "darkgrey")
+  # print(head(my_ccf[j,1:25]/max(my_ccf[j,])))
   # print(paste("Optimal T = ",((which(my_acf[i,j,]/max(my_acf[i,j,]))==0.5)*2)))
 } 
